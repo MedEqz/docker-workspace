c4a6488c076e15eb4c5fe07ea360e70381635dd4
medEqz <mehdi.boujenan@gmail.com>
2020-05-30 23:00:06 +0200

    chore: delete genererics

--- docker_project_3/nginx/right.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Changer directory permission Storage
-chgrp -R www-data storage bootstrap/cache
-chmod -R ug+rwx storage bootstrap/cache
-
--- docker_project_3/sails.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker exec -it project_3_sails /bin/bash
-
-
-
-
